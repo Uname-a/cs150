44df6eebc8a88c4d9e09889d24b9c80beda02c70
Matthew Baird <billybob2435@gmail.com>
2015-03-18 22:33:39 -0500

    3-16-15

---
+++ fish/config.fish
@@ -0,0 +1,19 @@
+# Path to your oh-my-fish.
+set fish_path $HOME/.oh-my-fish
+
+# Theme
+set fish_theme agnoster
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
+# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
+# Example format: set fish_plugins bundler z
+
+# Path to your custom folder (default path is ~/.oh-my-fish/custom)
+#set fish_custom $HOME/dotfiles/oh-my-fish
+
+# Load oh-my-fish configuration.
+. $fish_path/oh-my-fish.fish
+#alias idea='~/idea-IU-139.1117.1/bin/idea.sh'
+alias fuck='sudo apt-get remove --purge'
+alias ineed='sudo apt-get install'
+alias updt='sudo apt-get update ;  sudo apt-get upgrade'
